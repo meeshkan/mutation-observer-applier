a8ad77ad24dd15a90efa536e77120c644a98a352
k4m4 <nikolaskam@gmail.com>
2021-02-21 23:56:07 +0200

    Test mutation.type == 'childList' case

--- src/index.test.ts
+++ src/index.test.ts
@@ -111,6 +111,81 @@ const fixtures = {
       finalDom: '<html><head><title>Cool Website</title></head><body><h1>Welcome to the coolest website!</h1><p>Well, it\'s nothing more than another test website.</p></body></html>',
     },
   },
+  childList: {
+    add: {
+      initialDom: '<html><head><title>Cool Website</title></head><body><h1>Shopping List</h1><ul><li>Coffee</li><li>Tea</li><li>Milk</li></ul></body></html>',
+      mutations: [{
+        type: 'childList',
+        target: {
+          type: 1,
+          name: 'UL',
+          tagName: 'UL',
+          value: null,
+          outerHTML: '<ul><li>Coffee</li><li>Tea</li><li>Milk</li><li>Chocolate</li></ul>',
+          innerHTML: '<li>Coffee</li><li>Tea</li><li>Milk</li><li>Chocolate</li>',
+          attributes: {},
+          xpath: '/html/body/ul'
+        },
+        addedNodes: [{
+          type: 1,
+          name: 'LI',
+          tagName: 'LI',
+          value: null,
+          outerHTML: '<li>Chocolate</li>',
+          innerHTML: 'Chocolate',
+          attributes: {},
+          xpath: '/html/body/ul/li[4]'
+        }],
+        removedNodes: [],
+        previousSibling: {
+          type: 1,
+          name: 'LI',
+          tagName: 'LI',
+          value: null,
+          outerHTML: '<li>Milk</li>',
+          innerHTML: 'Milk',
+          attributes: {},
+          xpath: '/html/body/ul/li[3]'
+        },
+        nextSibling: null,
+        attributeName: null,
+        attributeNamespace: null
+      }],
+      finalDom: '<html><head><title>Cool Website</title></head><body><h1>Shopping List</h1><ul><li>Coffee</li><li>Tea</li><li>Milk</li><li>Chocolate</li></ul></body></html>',
+    },
+    remove: {
+      initialDom: '<html><head><title>Cool Website</title></head><body><h1>Shopping List</h1><ul><li>Coffee</li><li>Tea</li><li>Milk</li></ul></body></html>',
+      mutations: [{
+        type: 'childList',
+        target: {
+          type: 1,
+          value: null,
+          outerHTML: '<ul><li>Coffee</li><li>Tea</li></ul>',
+          attributes: {},
+          xpath: '/html/body/ul',
+        },
+        addedNodes: [],
+        removedNodes: [{
+          type: 1,
+          value: null,
+          outerHTML: '<li>Milk</li>',
+          attributes: {},
+          xpath: '/li',
+        }],
+        previousSibling: {
+          type: 1,
+          value: null,
+          outerHTML: '<li>Tea</li>',
+          attributes: {},
+          xpath: '/html/body/ul/li[2]',
+        },
+        nextSibling: null,
+        attributeName: null,
+        attributeNamespace: null,
+      }],
+      finalDom: '<html><head><title>Cool Website</title></head><body><h1>Shopping List</h1><ul><li>Coffee</li><li>Tea</li></ul></body></html>',
+    },
+  },
 };
 
 describe('applyMutations()', () => {
@@ -144,5 +219,18 @@ describe('applyMutations()', () => {
     expect(mod.DOM).toBe(fixtures.characterData.remove.initialDom);
     mod.applyMutations(fixtures.characterData.remove.mutations);
     expect(mod.DOM).toBe(fixtures.characterData.remove.finalDom);
+  });
+
+  it('adds elements to the DOM tree', () => {
+    const mod = new MutationObserverDiff(fixtures.childList.add.initialDom);
+    expect(mod.DOM).toBe(fixtures.childList.add.initialDom);
+    mod.applyMutations(fixtures.childList.add.mutations);
+    expect(mod.DOM).toBe(fixtures.childList.add.finalDom);
+  });
+  it('removes elements from the DOM tree', () => {
+    const mod = new MutationObserverDiff(fixtures.childList.remove.initialDom);
+    expect(mod.DOM).toBe(fixtures.childList.remove.initialDom);
+    mod.applyMutations(fixtures.childList.remove.mutations);
+    expect(mod.DOM).toBe(fixtures.childList.remove.finalDom);
   });
 });
