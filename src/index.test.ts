a9c59fa5175e3e6b64a9285ae282fee4425ca9e3
k4m4 <nikolaskam@gmail.com>
2021-02-21 22:03:20 +0200

    Test mutation.type == 'characterData' case

--- src/index.test.ts
+++ src/index.test.ts
@@ -67,6 +67,50 @@ const fixtures = {
       finalDom: '<html><head><title>Cool Website</title></head><body><h1>Welcome to the coolest website!</h1><p>Well, it\'s actually nothing more than another test website...</p></body></html>',
     },
   },
+  characterData: {
+    add: {
+      initialDom: '<html><head><title>Cool Website</title></head><body><h1>Welcome to the coolest website!</h1><p>Well, it\'s actually nothing more than another test website...</p></body></html>',
+      mutations: [{
+        type: 'characterData',
+        target: {
+          type: 3,
+          value: 'Well, it\'s actually nothing more than another test website... Don\'t act so surprised!',
+          attributes: {},
+          xpath: '',
+          parentXPath: '/html/body/p',
+          data: 'Well, it\'s actually nothing more than another test website... Don\'t act so surprised!'
+        },
+        addedNodes: [],
+        removedNodes: [],
+        previousSibling: null,
+        nextSibling: null,
+        attributeName: null,
+        attributeNamespace: null
+      }],
+      finalDom: '<html><head><title>Cool Website</title></head><body><h1>Welcome to the coolest website!</h1><p>Well, it\'s actually nothing more than another test website... Don\'t act so surprised!</p></body></html>',
+    },
+    remove: {
+      initialDom: '<html><head><title>Cool Website</title></head><body><h1>Welcome to the coolest website!</h1><p>Well, it\'s actually nothing more than another test website...</p></body></html>',
+      mutations: [{
+        type: 'characterData',
+        target: {
+          type: 3,
+          value: 'Well, it\'s nothing more than another test website.',
+          attributes: {},
+          xpath: '',
+          parentXPath: '/html/body/p',
+          data: 'Well, it\'s nothing more than another test website.'
+        },
+        addedNodes: [],
+        removedNodes: [],
+        previousSibling: null,
+        nextSibling: null,
+        attributeName: null,
+        attributeNamespace: null
+      }],
+      finalDom: '<html><head><title>Cool Website</title></head><body><h1>Welcome to the coolest website!</h1><p>Well, it\'s nothing more than another test website.</p></body></html>',
+    },
+  },
 };
 
 describe('applyMutations()', () => {
@@ -87,5 +131,18 @@ describe('applyMutations()', () => {
     expect(mod.DOM).toBe(fixtures.attributes.remove.initialDom);
     mod.applyMutations(fixtures.attributes.remove.mutations);
     expect(mod.DOM).toBe(fixtures.attributes.remove.finalDom);
+  });
+
+  it('adds character data', () => {
+    const mod = new MutationObserverDiff(fixtures.characterData.add.initialDom);
+    expect(mod.DOM).toBe(fixtures.characterData.add.initialDom);
+    mod.applyMutations(fixtures.characterData.add.mutations);
+    expect(mod.DOM).toBe(fixtures.characterData.add.finalDom);
+  });
+  it('removes character data', () => {
+    const mod = new MutationObserverDiff(fixtures.characterData.remove.initialDom);
+    expect(mod.DOM).toBe(fixtures.characterData.remove.initialDom);
+    mod.applyMutations(fixtures.characterData.remove.mutations);
+    expect(mod.DOM).toBe(fixtures.characterData.remove.finalDom);
   });
 });
