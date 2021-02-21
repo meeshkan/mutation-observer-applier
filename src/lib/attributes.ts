ddea30bc3329f4beb1ad6c55028e6155011dc654
k4m4 <nikolaskam@gmail.com>
2021-02-21 18:57:06 +0200

    Handle elements without attrs

--- src/lib/attributes.ts
+++ src/lib/attributes.ts
@@ -4,6 +4,10 @@ export type IAttributes = {
 
 export const getAttributes = (element: any): IAttributes => {
     const attributesObject: IAttributes = {};
+    if (!element || !element.attributes) {
+        return {};
+    }
+
     for (let attribute, i = 0, attributes = element.attributes, n = attributes.length; i < n; i++) {
         attribute = attributes[i];
         attributesObject[attribute.nodeName] = attribute.nodeValue;
