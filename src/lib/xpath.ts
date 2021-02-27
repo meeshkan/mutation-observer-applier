d32353bd632a7e9ce9b63d0eef165e7a9534e523
k4m4 <nikolaskam@gmail.com>
2021-02-27 21:17:16 +0200

    Fix getXPath for text & comment node types

--- src/lib/xpath.ts
+++ src/lib/xpath.ts
@@ -1,23 +1,30 @@
+const validNodeTypes = [
+    Node.ELEMENT_NODE,
+    Node.TEXT_NODE,
+    Node.COMMENT_NODE,
+];
+
 // Source (modified): https://github.com/thiagodp/get-xpath/blob/master/src/index.ts
-export const getXPath = (element: any): string => {
-    let nodeElement = element;
+export const getXPath = (element: Node): string => {
     let parts: string[] = [];
-    while (nodeElement && nodeElement.nodeType === Node.ELEMENT_NODE) {
-        let nbOfPreviousSiblings = 0;
+    while (element && validNodeTypes.includes(element.nodeType)) {
+        let numberOfPreviousSiblings = 0;
         let hasNextSiblings = false;
-        let sibling = nodeElement.previousSibling;
+        let sibling = element.previousSibling;
         while (sibling) {
-            if (sibling.nodeType !== Node.DOCUMENT_TYPE_NODE &&
-                sibling.nodeName === nodeElement.nodeName) {
-                nbOfPreviousSiblings++;
+            if (
+                sibling.nodeType !== Node.DOCUMENT_TYPE_NODE &&
+                sibling.nodeName === element.nodeName
+            ) {
+                numberOfPreviousSiblings++;
             }
 
             sibling = sibling.previousSibling;
         }
 
-        sibling = nodeElement.nextSibling;
+        sibling = element.nextSibling;
         while (sibling) {
-            if (sibling.nodeName === nodeElement.nodeName) {
+            if (sibling.nodeName === element.nodeName) {
                 hasNextSiblings = true;
                 break;
             }
@@ -25,13 +32,20 @@ export const getXPath = (element: any): string => {
             sibling = sibling.nextSibling;
         }
 
-        let prefix = nodeElement.prefix ? nodeElement.prefix + ':' : '';
-        let nth = nbOfPreviousSiblings || hasNextSiblings
-            ? '[' + (nbOfPreviousSiblings + 1) + ']'
-            : '';
+        let part: string;
+        if ([Node.TEXT_NODE, Node.COMMENT_NODE].includes(element.nodeType)) {
+            part = element.nodeName.slice(1) + '()';
+        } else {
+            let prefix = element.prefix ? element.prefix + ':' : '';
+            let nth = numberOfPreviousSiblings || hasNextSiblings
+                ? '[' + (numberOfPreviousSiblings + 1) + ']'
+                : '';
+            
+            part = prefix + element.localName + nth;
+        }
 
-        parts.push(prefix + nodeElement.localName + nth);
-        nodeElement = nodeElement.parentNode;
+        parts.push(part);
+        element = element.parentNode;
     }
 
     return parts.length ? '/' + parts.reverse().join('/') : '';
