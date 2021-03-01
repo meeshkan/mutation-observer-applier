0f0a8822a3235c7881dc71540f780952a4ee932d
k4m4 <nikolaskam@gmail.com>
2021-03-01 14:08:32 +0200

    Create object for Node types in XPath lib

--- src/lib/xpath.ts
+++ src/lib/xpath.ts
@@ -1,3 +1,13 @@
+const Node = {
+    ELEMENT_NODE: 1,
+    ATTRIBUTE_NODE: 2,
+    TEXT_NODE: 3,
+    COMMENT_NODE: 8,
+    DOCUMENT_NODE: 9,
+    DOCUMENT_TYPE_NODE: 10,
+    DOCUMENT_FRAGMENT_NODE: 11,
+};
+
 const validNodeTypes = [
     Node.ELEMENT_NODE,
     Node.TEXT_NODE,
